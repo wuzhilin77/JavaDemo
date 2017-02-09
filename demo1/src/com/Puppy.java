56a8a722be34ebf8fed62657ad0dc5e98412b885
wuzhilin77 <834398483@qq.com>
2017-02-09 19:22:39 +0800

    提交代码

--- demo1/src/com/Puppy.java
+++ demo1/src/com/Puppy.java
@@ -1,12 +1,20 @@
 package com;
 public class Puppy{
-   public Puppy(String name){
-      //这个构造器仅有一个参数：name
-      System.out.println("小狗的名字是 : " + name ); 
-   }
-   public static void main(String []args){
-      // 下面的语句将创建一个Puppy对象
-      @SuppressWarnings("unused")
-	Puppy Mypuppy = new Puppy( "tommy" );
-   }
+	int puppyAge;
+	public Puppy(String name){
+		System.out.println("小狗的名字是；" + name);
+	}
+	public void setAge(int age){
+		puppyAge=age;
+	}
+	public int getAge(){
+		System.out.println("小狗的年龄为；"+puppyAge);
+		return puppyAge;
+	}
+	public static void main(String []args){
+	Puppy myPuppy = new Puppy("tommy");
+	myPuppy.setAge(2); 
+	myPuppy.getAge();
+	System.out.println("变量值 ；" + myPuppy.puppyAge);
+	}
 }
