f1b0da03f6edcf73d29de8c706364c0a3cbb5754
wuzhilin77 <834398483@qq.com>
2017-02-10 17:19:08 +0800

    00

---
+++ demo1/src/com/FuDian.java
@@ -0,0 +1,12 @@
+package com;
+
+public class FuDian {
+	public static void main(String[] args){
+		float f=1.23f;//定义一个不带后缀的单精度浮点类型
+		double d1=1.23;//定义一个不带后缀的双精度浮点类型
+		double d2=1023d;//定义一个带后缀的双精度浮点类型
+		System.out.println("单精度浮点类型数值等于"+f);
+		System.out.println("双精度浮点类型数值等于"+d1);
+		System.out.println("双精度浮点类型数值等于"+d2);
+	}
+}
