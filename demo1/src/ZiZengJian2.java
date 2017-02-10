4b403b732946e865a71cb02ced68767be9ffd609
wuzhilin77 <834398483@qq.com>
2017-02-10 21:24:37 +0800

    入门2.2

---
+++ demo1/src/ZiZengJian2.java
@@ -0,0 +1,11 @@
+public class ZiZengJian2 {
+	public static void main(String[] args) {
+		byte b1 = 5;
+		byte b2 = (byte) (b1 + 1);
+		System.out.println("使用加运算符的结果是" + b2);
+		byte b3 = 5;
+		byte b4 = ++b3;
+		System.out.println("使用自增运算符的结果是" + b4);
+	}
+
+}
