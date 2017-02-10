84d4fa94e2c2c287d61bfeccc816212b6880d404
wuzhilin77 <834398483@qq.com>
2017-02-10 15:37:01 +0800

    入门

---
+++ demo1/src/com/JinZhi.java
@@ -0,0 +1,10 @@
+package com;
+
+public class JinZhi {
+	public static void main(String[] args) {
+		int a10 = 12;// 定义一个十进制数值
+		int a8 = 012;// 定义一个八进制数值
+		System.out.println("十进制12等于" + a10);
+		System.out.println("八进制12等于" + a8);
+	}
+}
---
+++ demo1/src/com/JinZhi16.java
@@ -0,0 +1,11 @@
+package com;
+
+public class JinZhi16 {
+	public static void main(String[] args) {
+		int a1 = 0x12;// 定义一个以数字表示的十六进制整数
+		int a2 = 0xcafe;// 定义一个以字母表示的十六进制整数
+		System.out.println("第一个十六进制数值等于" + a1);
+		System.out.println("第二个十六进制数值等于" + a2);
+	}
+
+}
