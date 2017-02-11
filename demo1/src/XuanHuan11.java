78871764ff4b093e12eabae6433bc174eec6007e
wuzhilin77 <834398483@qq.com>
2017-02-12 02:11:20 +0800

    入门3.2

---
+++ demo1/src/XuanHuan11.java
@@ -0,0 +1,12 @@
+public class XuanHuan11 {
+	public static void main(String[] args) {
+		out: for (int i = 0; i < 10; i++)
+			for (int j = 0; j < 10; j++)
+				if (j >= i) {
+					System.out.println();
+					continue out;
+				} else
+					System.out.print("i=" + i + "j=" + j);
+	}
+
+}
